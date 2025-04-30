a8e704b86e46f97006f66c961d878c46eead6443
DESKTOP-EVP3BIN\11917 <1191755065@qq.com>
2025-04-29 19:01:13 -0700

    Added javascript file - app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const contentText = document.querySelector('.content');
-
-document.addEventListener('DOMContentLoaded', function () {
-    contentText.innerHTML = "This is the content";
-}, false);
